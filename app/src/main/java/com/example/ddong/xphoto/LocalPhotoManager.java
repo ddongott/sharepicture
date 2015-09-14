1ac10bbfd78a73edf073434e9df7f1d141c5947a
ddong <davidgwdong@gmail.com>
2015-09-13 21:19:16 -0400

    Initial commit 1: 3 empty tables: protected photo, shared photo, contacts. 2: facebook login work, but not enabled. 3: grid view for local photos, 4: photo details,(not work, because use bitmap as parameter, which cause binder transition failure, should use url).

--- app/src/main/java/com/example/ddong/xphoto/LocalPhotoManager.java
+++ app/src/main/java/com/example/ddong/xphoto/LocalPhotoManager.java
@@ -1,18 +1,54 @@
 package com.example.ddong.xphoto;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class LocalPhotoManager extends AppCompatActivity {
-
+    private GridView gridView;
+    private LocalGridViewAdapter gridAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_manager);
+
+        gridView = (GridView) findViewById(R.id.gridView);
+        gridAdapter = new LocalGridViewAdapter(this, R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                ImageItem item = (ImageItem) parent.getItemAtPosition(position);
+                //Create intent
+                Intent intent = new Intent(LocalPhotoManager.this, PhotoDetailsActivity.class);
+                intent.putExtra("title", item.getTitle());
+                intent.putExtra("image", item.getImage());
+
+                //Start details activity
+                startActivity(intent);
+            }
+        });
     }
 
+    // Prepare some dummy data for gridview
+    private ArrayList<ImageItem> getData() {
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        }
+        return imageItems;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/example/ddong/xphoto/PhotoDetailsActivity.java
@@ -0,0 +1,25 @@
+package com.example.ddong.xphoto;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PhotoDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_details);
+
+        String title = getIntent().getStringExtra("title");
+        Bitmap bitmap = getIntent().getParcelableExtra("image");
+
+        TextView titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setText(title);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        imageView.setImageBitmap(bitmap);
+    }
+}
